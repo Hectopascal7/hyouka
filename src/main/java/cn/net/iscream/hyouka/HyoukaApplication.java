dfbc1355a150abfc04f1f3792696ea0b460460eb
Hectopascal7 <hectopascal7@yeah.net>
2020-03-05 21:43:21 +0800

    hyouka初始化。

---
+++ src/main/java/cn/net/iscream/hyouka/HyoukaApplication.java
@@ -0,0 +1,13 @@
+package cn.net.iscream.hyouka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyoukaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyoukaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/net/iscream/hyouka/HyoukaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.net.iscream.hyouka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyoukaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
